afa8c518fbdccd2d84835ae0d408c99e65a78c6f
Supreet Sharma <supreet.shm523@gmail.com>
2018-04-11 00:07:39 +0530

    kamals encryption problem

---
+++ AlphaEncryption.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class AlphaEncryption {
+
+	public static char getDecodedChar(char a) {
+		int decoded = (int) a + 3;
+		if (decoded > 122)
+			return (char) (decoded - 26);
+		return (char) decoded;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String in = scan.nextLine();
+		StringBuilder out = new StringBuilder();
+		List<Character> charList = in.chars().mapToObj(e -> (char) e).collect(Collectors.toList());
+		charList.forEach(li -> out.append(getDecodedChar(li)));
+		System.out.println(out);
+		scan.close();
+	}
+
+}
